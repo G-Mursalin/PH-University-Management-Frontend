750ce6e978c3cba3ffcdffdaaaa06568aafdf185
Golam Mursalin <golammursalin309@gmail.com>
2024-01-16 09:46:22 +0600

    project initialized

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,77 @@
+import { Layout, Menu } from "antd";
+import type { MenuProps } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
+
+const items: MenuProps["items"] = [
+  {
+    key: "1",
+    label: "Dashboard",
+  },
+  {
+    key: "2",
+    label: "Profile",
+  },
+  {
+    key: "3",
+    label: "User Management",
+    children: [
+      { key: "31", label: "Create Admin" },
+      { key: "32", label: "Create Student" },
+    ],
+  },
+];
+
+function MainLayout() {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "1rem 0 1rem 0",
+          }}
+        >
+          <h1>PH University</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          PH University Management ©{new Date().getFullYear()} Created by PH
+          University
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+}
+
+export default MainLayout;
