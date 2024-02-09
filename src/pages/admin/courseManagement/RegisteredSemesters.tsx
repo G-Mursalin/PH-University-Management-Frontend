59c91e0b7697bb4bb8098fdb166ce6cef105d767
Golam Mursalin <golammursalin309@gmail.com>
2024-02-09 22:21:27 +0600

    Add registered semesters table view

--- src/pages/admin/courseManagement/RegisteredSemesters.tsx
+++ src/pages/admin/courseManagement/RegisteredSemesters.tsx
@@ -1,5 +1,124 @@
-function RegisteredSemesters() {
-  return <div>RegisteredSemesters</div>;
-}
+import {
+  Button,
+  Dropdown,
+  MenuProps,
+  Table,
+  TableColumnsType,
+  Tag,
+} from "antd";
+import { useState } from "react";
+import { toast } from "sonner";
+import {
+  useGetAllRegisteredSemestersQuery,
+  useUpdateRegisteredSemesterMutation,
+} from "../../../redux/features/admin/courseManagement";
+import { TSemester } from "../../../types";
+export type TTableData = Pick<TSemester, "startDate" | "endDate" | "status">;
+
+const items = [
+  {
+    label: "Upcoming",
+    key: "UPCOMING",
+  },
+  {
+    label: "Ongoing",
+    key: "ONGOING",
+  },
+  {
+    label: "Ended",
+    key: "ENDED",
+  },
+];
+
+const RegisteredSemesters = () => {
+  const [semesterId, setSemesterId] = useState("");
+  const { data: semesterData, isFetching } =
+    useGetAllRegisteredSemestersQuery(undefined);
+
+  const [updateSemesterStatus] = useUpdateRegisteredSemesterMutation();
+
+  const tableData = semesterData?.data?.map(
+    ({ _id, academicSemester, startDate, endDate, status }) => ({
+      key: _id,
+      name: `${academicSemester.name} ${academicSemester.year}`,
+      startDate: startDate.substring(0, 10),
+      endDate: endDate.substring(0, 10),
+      status,
+    })
+  );
+
+  const handleStatusUpdate: MenuProps["onClick"] = async (data) => {
+    console.log(data);
+    const toastId = toast.loading("Updating...");
+    const updateData = {
+      id: semesterId,
+      data: {
+        status: data.key,
+      },
+    };
+
+    // Send to server
+    try {
+      await updateSemesterStatus(updateData).unwrap();
+      toast.success("Updated Successfully", { id: toastId });
+    } catch (error) {
+      toast.error("Something Wend Wrong!", { id: toastId });
+    }
+  };
+
+  const menuProps = {
+    items,
+    onClick: handleStatusUpdate,
+  };
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      render: (item) => {
+        let color;
+        if (item === "UPCOMING") {
+          color = "blue";
+        }
+        if (item === "ONGOING") {
+          color = "green";
+        }
+        if (item === "ENDED") {
+          color = "red";
+        }
+
+        return <Tag color={color}>{item}</Tag>;
+      },
+    },
+    {
+      title: "Start Date",
+      dataIndex: "startDate",
+    },
+    {
+      title: "End Date",
+      dataIndex: "endDate",
+    },
+    {
+      title: "Action",
+      render: (item) => {
+        return (
+          <Dropdown menu={menuProps} trigger={["click"]}>
+            <Button onClick={() => setSemesterId(item.key)}>
+              Update Status
+            </Button>
+          </Dropdown>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Table loading={isFetching} columns={columns} dataSource={tableData} />
+  );
+};
 
 export default RegisteredSemesters;
