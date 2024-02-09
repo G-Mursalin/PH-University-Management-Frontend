7d450bd3e78aa2d11573da6ae4020fed064a82af
Golam Mursalin <golammursalin309@gmail.com>
2024-02-10 00:01:32 +0600

    Add courses table view

--- src/pages/admin/courseManagement/Courses.tsx
+++ src/pages/admin/courseManagement/Courses.tsx
@@ -1,5 +1,110 @@
-function Courses() {
-  return <div>Courses</div>;
-}
+import { Button, Modal, Table } from "antd";
+import { useState } from "react";
+import { FieldValues } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import PHSelect from "../../../components/form/PHSelect";
+import {
+  useAddFacultiesMutation,
+  useGetAllCoursesQuery,
+} from "../../../redux/features/admin/courseManagement";
+import { useGetAllFacultiesQuery } from "../../../redux/features/admin/userManagement";
+import { toast } from "sonner";
+
+type TRowData = {
+  key: string;
+  title: string;
+  code: string;
+};
+
+const Courses = () => {
+  const { data: courses, isFetching } = useGetAllCoursesQuery(undefined);
+
+  const tableData = courses?.data?.map(({ _id, title, prefix, code }) => ({
+    key: _id,
+    title,
+    code: `${prefix}${code}`,
+  }));
+
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Code",
+      dataIndex: "code",
+      key: "code",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (item: TRowData) => {
+        return <AddFacultyModal facultyInfo={item} />;
+      },
+    },
+  ];
+
+  return (
+    <Table loading={isFetching} columns={columns} dataSource={tableData} />
+  );
+};
+
+// Modal Component
+const AddFacultyModal = ({ facultyInfo }: { facultyInfo: TRowData }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const { data: facultiesData } = useGetAllFacultiesQuery(undefined);
+  const [addFaculties] = useAddFacultiesMutation();
+
+  const facultiesOption = facultiesData?.data?.map((item) => ({
+    value: item._id,
+    label: item.fullName,
+  }));
+
+  const handleSubmit = async (data: FieldValues) => {
+    const toastId = toast.loading("Assigning Faculties...");
+    const facultyData = {
+      courseId: facultyInfo!.key,
+      data,
+    };
+    try {
+      await addFaculties(facultyData).unwrap();
+      handleCancel();
+      toast.success("Faculties Assigned Successfully", { id: toastId });
+    } catch (error) {
+      toast.error("Something Wend Wrong", { id: toastId });
+    }
+  };
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      <Button onClick={showModal}>Add Faculty</Button>
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <PHForm onSubmit={handleSubmit}>
+          <PHSelect
+            mode="multiple"
+            options={facultiesOption}
+            name="faculties"
+            label="Faculty"
+          />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Modal>
+    </>
+  );
+};
 
 export default Courses;
